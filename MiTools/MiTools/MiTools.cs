cdc725836879341071f426be633550733bd3226b
푸른곡괭이 <sungwanim@naver.com>
2019-02-03 13:35:55 +0900

    Update MiTools.cs

--- MiTools/MiTools/MiTools.cs
+++ MiTools/MiTools/MiTools.cs
@@ -5,7 +5,7 @@ using MiNET.Plugins.Attributes;
 
 namespace MiTools
 {
-    [Plugin(PluginName = "MiTools", Description = "Tools Kit in MiNET", Author = "푸른곡괭이(BluePickaxe)", PluginVersion = "0.1 beta")]
+    [Plugin(PluginName = "MiTools", Description = "Tools Kit in MiNET", Author = "BluePickaxe", PluginVersion = "0.1 beta")]
     public class MiTools : Plugin
     {
         
